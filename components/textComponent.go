9d7076d3a05b32c8a1d17fa539fd01e37bd4d901
xfrogcn <xfrogcn@163.com>
2020-06-19 17:53:29 +0800

    文本组件

--- components/textComponent.go
+++ components/textComponent.go
@@ -1,28 +1,40 @@
 package components
 
 import (
+	"fmt"
 	"image"
+	"math"
 	"strings"
 	"unicode"
 
-	"github.com/llgcode/draw2d/draw2dimg"
+	"github.com/fogleman/gg"
 )
 
 type (
 	// TextComponentDefine 文本组件
 	TextComponentDefine struct {
 		ShapeComponentDefine
-		Text     string
-		FontSize int
+		Text       string
+		FontPath   string
+		FontSize   float64
+		LineHeight float64
 	}
 
 	// TextComponent 文本组件
 	TextComponent struct {
+		_drawItmes [][]drawTextItem
+		_w, _h     float64
+	}
+
+	drawTextItem struct {
+		x, y, w, h, b float64
+		text          string
 	}
 )
 
 func splitText(s string) [][]rune {
 	t := strings.Replace(s, "\r\n", "\n", -1)
+	t = strings.Replace(t, "\r", "\n", -1)
 	ts := []rune(t)
 
 	text := make([][]rune, 0)
@@ -32,11 +44,11 @@ func splitText(s string) [][]rune {
 			if word != "" {
 				text = append(text, []rune(word))
 			}
-			if tx == '\t' {
-				text = append(text, []rune("    "))
-			} else {
-				text = append(text, []rune{tx})
-			}
+			// if tx == '\t' {
+			// 	text = append(text, []rune("    "))
+			// } else {
+			text = append(text, []rune{tx})
+			//}
 
 			word = ""
 		} else {
@@ -51,72 +63,199 @@ func splitText(s string) [][]rune {
 	return text
 }
 
-func measureText(gc *draw2dimg.GraphicContext, text [][]rune, w float64) {
-	y := 0.0
+func measureText(gc *gg.Context, text [][]rune, w float64, lineHeight float64) [][]drawTextItem {
+	x, y := 0.0, 0.0
+	dl := make([]drawTextItem, 0)
+
+	_, norHeight := gc.MeasureString("国")
+
 	for _, rs := range text {
-		_, _, r, b := gc.GetStringBounds(string(rs))
-		si := 0
-		if r > w {
-			for idx := range rs {
-				_, _, r, b = gc.GetStringBounds(string(rs[si : idx+1]))
-				if r > w {
-					y = b
-					si = idx
-					gc.MoveTo(0.0, y)
+		if len(rs) == 1 && rs[0] == '\n' {
+			// 直接换行
+			dl = append(dl, drawTextItem{
+				x:    0,
+				y:    y,
+				w:    0,
+				h:    norHeight,
+				text: "",
+			})
+			y++
+			x = 0
+			continue
+		}
+		tw, th := gc.MeasureString(string(rs))
+
+		if (x + tw) > w {
+			//换行
+			if tw > w {
+				//强拆
+				sr := 0
+				for i := sr; i < len(rs); i++ {
+					s := string(rs[sr : i+1])
+					sw, sh := gc.MeasureString(s)
+					if (x + sw) > w {
+						s = string(rs[sr:i])
+						sw, sh = gc.MeasureString(s)
+						sr = i
+						dl = append(dl, drawTextItem{
+							x:    x,
+							y:    y,
+							w:    sw,
+							h:    sh,
+							text: s,
+						})
+						y++
+						x = 0
+					}
+
+					if i == (len(rs) - 1) {
+						dl = append(dl, drawTextItem{
+							x:    x,
+							y:    y,
+							w:    sw,
+							h:    sh,
+							text: s,
+						})
+						x += sw
+					}
 				}
+
+			} else {
+				y++
+				x = 0
+				dl = append(dl, drawTextItem{
+					x:    x,
+					y:    y,
+					w:    tw,
+					h:    th,
+					text: string(rs),
+				})
+				x += tw
 			}
+		} else {
+
+			dl = append(dl, drawTextItem{
+				x:    x,
+				y:    y,
+				w:    tw,
+				h:    th,
+				text: string(rs),
+			})
+			x += tw
 		}
 
 	}
+
+	lines := make([][]drawTextItem, 0)
+
+	// lh := 0.0
+	y = 0
+	top := 0.0
+	si := 0
+
+	processLine := func(line []drawTextItem, top float64) ([]drawTextItem, float64) {
+		lh := 0.0
+		for _, di := range line {
+			lh = math.Max(lh, di.h)
+		}
+		// 行高
+		alh := lh
+		if lineHeight > alh {
+			alh = lineHeight
+		}
+
+		newItems := make([]drawTextItem, 0)
+		for _, item := range line {
+			item.h = alh
+			item.y = (top + alh) - (alh-lh)/2.0
+			item.b = top + alh
+			newItems = append(newItems, item)
+		}
+
+		return newItems, top + alh
+
+	}
+
+	for idx, di := range dl {
+		if di.y != y {
+			items := dl[si:idx]
+			var newItems []drawTextItem
+			newItems, top = processLine(items, top)
+			lines = append(lines, newItems)
+			y = di.y
+			si = idx
+		}
+		if idx == len(dl)-1 {
+			items := dl[si : idx+1]
+			var newItems []drawTextItem
+			newItems, top = processLine(items, top)
+			lines = append(lines, newItems)
+		}
+
+	}
+
+	return lines
 }
 
 // Draw 绘制
 func (c *TextComponent) Draw(dc *DrawContext, config interface{}) error {
-	// cd := config.(*TextComponentDefine)
+	cd := config.(*TextComponentDefine)
 
-	// var err error = nil
-	// fc, err := cd.ShapeComponentDefine.FillColor.Parse()
-	// if err != nil {
-	// 	return err
-	// }
-	// sc, err := cd.ShapeComponentDefine.StrokeColor.Parse()
-	// if err != nil {
-	// 	return err
-	// }
+	var err error = nil
 
-	//lw := float64(cd.ShapeComponentDefine.LineWidth)
-	//gc := dc.GraphicContext
-	// gc.Save()
-	// gc.SetLineWidth(lw)
-	// gc.SetFillColor(fc)
-	// gc.SetStrokeColor(sc)
-	// gc.SetFillRule(draw2d.FillRuleWinding)
-	// gc.BeginPath()
+	dc.GraphicContext.Push()
+	defer dc.GraphicContext.Pop()
+	if err = dc.GraphicContext.LoadFontFace(cd.FontPath, cd.FontSize); err != nil {
+		return err
+	}
 
-	// //x := float64(cd.Radius)
-	// //gc.ArcTo(float64(x), float64(x), float64(cd.Radius)-lw/2, float64(cd.Radius)-lw/2, 0, 2*math.Pi)
-	// gc.FillStroke()
+	if fc, err := cd.FillColor.Parse(); err == nil {
+		dc.GraphicContext.SetColor(fc)
+	} else {
+		return err
+	}
 
-	// gc.Restore()
+	lines := c._drawItmes
+	for _, l := range lines {
+		for _, t := range l {
+			dc.GraphicContext.DrawString(t.text, t.x, t.y)
+		}
+	}
 
-	return nil
+	return err
 }
 
 // Measure 测量
 func (c *TextComponent) Measure(dc *DrawContext, rect image.Rectangle, config interface{}) (image.Rectangle, error) {
-	//td := config.(*TextComponentDefine)
-	//dc.GraphicContext.Save()
-	//dc.GraphicContext.siz. SetFontSize(float64(td.FontSize))
-	dc.GraphicContext.MoveTo(0, 0)
-	// w, h := rect.Dx(), rect.Dy()
-	//w := float64(rect.Dx())
-	//y := 0.0
+	cd := config.(*TextComponentDefine)
 
-	//text := splitText(td.Text)
+	var err error = nil
+	rt := image.Rect(0, 0, 0, 0)
 
-	//dc.GraphicContext.getstr
+	w := rect.Dx()
+	if w <= 0 {
+		return rt, fmt.Errorf("必须确定宽度")
+	}
 
-	return image.Rect(0, 0, 0, 0), nil
+	if err = dc.GraphicContext.LoadFontFace(cd.FontPath, cd.FontSize); err != nil {
+		return rt, err
+	}
+
+	texts := splitText(cd.Text)
+	lines := measureText(dc.GraphicContext, texts, float64(w), cd.LineHeight)
+	c._drawItmes = lines
+
+	mw, mh := 0.0, 0.0
+	for _, l := range lines {
+
+		for _, t := range l {
+			mw = math.Max(mw, t.x+t.w)
+			mh = math.Max(mh, t.b)
+		}
+	}
+	c._w = mw
+	c._h = mh
+	return image.Rect(0, 0, int(mw), int(mh)), nil
 }
 
 // ConfigType 配置类型
--- components/textComponent_test.go
+++ components/textComponent_test.go
@@ -1,10 +1,40 @@
 package components
 
 import (
+	"image"
+	"image/color"
+	"image/png"
+	"os"
 	"testing"
 
+	"github.com/fogleman/gg"
 	"github.com/go-playground/assert/v2"
 )
+
+var text = ComponentDefine{
+	Type:  "text",
+	Level: 1,
+	Area: Rectangle{
+		Left:   "0",
+		Top:    "0",
+		Right:  "200",
+		Bottom: "100%",
+	},
+	Position:        "center",
+	Size:            "contain",
+	Repeat:          RepeatNO,
+	Padding:         "0",
+	BackgroundColor: "#000000",
+	ComponentData: map[string]interface{}{
+		"fillColor":   "#ff0000",
+		"strokeColor": "#00ff00",
+		"lineWidth":   10,
+		"text":        "HELLO，\r\n你好，\r你在做什么？this is a long text begintextbegintextbegintextbegin",
+		"fontPath":    "../fonts/zkgdh.ttf",
+		"fontSize":    24,
+		"lineHeight":  32,
+	},
+}
 
 func TestSplitText(t *testing.T) {
 	s := "abc"
@@ -29,22 +59,32 @@ func TestSplitText(t *testing.T) {
 	assert.Equal(t, string(ms[5]), "家")
 	assert.Equal(t, string(ms[6]), "、")
 	assert.Equal(t, string(ms[7]), "🏠")
-	assert.Equal(t, string(ms[8]), "    ")
+	assert.Equal(t, string(ms[8]), "\t")
 }
 
-func TestMeasureText(t *testing.T) {
-	// s := "a国 b\r\n家、🏠\t"
-	// ms := splitText(s)
+func TestDrawText(t *testing.T) {
+	bg := image.NewRGBA(image.Rect(0, 0, 400, 500))
+	gc := gg.NewContextForRGBA(bg)
+	gc.SetColor(color.Black)
+	gc.Clear()
 
-	//bg := image.NewRGBA(image.Rect(0, 0, 100, 100))
-	//gc := draw2dimg.NewGraphicContext(bg)
+	cd := text
+	cd.Size = "100% 100%"
+	//cd.Repeat = RepeatXY
 
-	// draw2d.SetFontFolder("../fonts")
+	dc := &DrawContext{
+		GraphicContext: gc,
+		Image:          bg,
+		Width:          400,
+		Height:         500,
+		CurrentLeft:    0,
+		CurrentTop:     0,
+	}
 
-	// gc.SetFontSize(12)
+	dc.DrawComponent(cd)
+	f, _ := os.Create("../test/component_text_center.png")
+	png.Encode(f, gc.Image())
 
-	// measureText(gc, ms, 100)
-
-	//g := exp..
+	f.Close()
 
 }
